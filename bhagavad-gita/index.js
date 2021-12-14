145cb12f4a0245850ba5e58ca083e48b531b3e69
Kishore Annam <annamkishore@gmail.com>
2021-12-15 00:30:55 +0530

    basic page with slokas

--- bhagavad-gita/index.js
+++ bhagavad-gita/index.js
@@ -8,8 +8,11 @@ class RootElement extends React.Component {
     render() {
         return <>
             <h1>భగవద్గీత</h1>
+            <a href={"https://raw.githubusercontent.com/annamkishore/annamkishore.github.io/79a994859fb6dce438b079c56bebb099b1f02581/bhagavad-gita/slokas.yaml"}>
+                Slokas link (for now)
+            </a>
             <h2> Chapter wise count </h2>
-            <img src={"./bhagavad-gita/stats.png"} />
+            <img src={"./bhagavad-gita/stats.png"}/> <br/>
         </>;
     }
 }
